857cfca1415747af2ee577010ed4515e73c78f28
David Neidhart <david.neidhart@gmx.net>
2023-12-07 18:31:38 +0100

    feat: day 6
    Signed-off-by: David Neidhart <david.neidhart@gmx.net>

--- aoc_6_12/src/main.rs
+++ aoc_6_12/src/main.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+struct Race {
+    pub time: u128,
+    pub distance: u128
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let filepath = "src/sample.txt";
+    let contents = std::fs::read_to_string(filepath).unwrap().leak();
+
+    let mut races: Vec<Race> = Vec::new();
+
+    let lines = contents.split('\n').map(|x| { x.split(":").nth(1).unwrap().trim() }).collect::<Vec<&str>>();
+
+    let mut time_line = lines[0].split_ascii_whitespace().collect::<Vec<&str>>().join("");
+    let mut distance_line = lines[1].split_ascii_whitespace().collect::<Vec<&str>>().join("");
+
+    let race = Race {
+        time: time_line.parse::<u128>().unwrap(),
+        distance: distance_line.parse::<u128>().unwrap()
+    };
+
+    let mut distances: Vec<u128> = Vec::new();
+
+    for i in 1..race.time {
+
+        let distance = (race.time - i) * i;
+
+        distances.push(distance);
+    }
+
+    let total = distances.iter().filter(|x| { x > &&race.distance }).count() as u128;
+
+    println!("{}", total);
 }
